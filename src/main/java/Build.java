6f3a881974e2be50d52094041ce100dcda26a3c9
caillouc <cailloucolson@gmail.com>
2021-02-06 13:22:46 +0100

    #8 clone method works, but for now only clone in current folder

--- src/main/java/Build.java
+++ src/main/java/Build.java
@@ -1,3 +1,10 @@
+import org.eclipse.jgit.api.Git;
+import org.eclipse.jgit.api.errors.GitAPIException;
+import org.eclipse.jgit.api.errors.JGitInternalException;
+import java.io.File;
+import java.io.IOException;
+import org.eclipse.jgit.util.FileUtils;
+
 /**
  * Contains information needed for the build and methods for building
  */
@@ -6,12 +13,27 @@ public class Build {
      * Constructs with properties about what (branch) to clone and build
      */
     public Build() {
+        // Now its not empty anymore
     }
 
     /**
      * Clones the specified repository branch into a local folder to prepare testing
      */
-    private void cloneRepo() {
+    public void cloneRepo() {
+        String repoUrl = "https://github.com/Atema/DD2480-CI.git";
+        try {
+            System.out.println("Cloning " + repoUrl + " into current folder");
+            Git.cloneRepository().setURI(repoUrl)
+                    // .setDirectory(directory)
+                    // .setDirectory(Paths.get(cloneDirectoryPath).toFile())
+                    .call();
+            System.out.println("Completed Cloning");
+        } catch (JGitInternalException e){
+            System.out.println("Repo alreay cloned");
+        }catch (GitAPIException e) {
+            System.out.println("Exception occurred while cloning repo");
+            e.printStackTrace();
+        }
     }
 
     /**
--- src/test/java/BuildTests.java
+++ src/test/java/BuildTests.java
@@ -1,6 +1,21 @@
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
 /**
  * Contains tests for the {@link Build} class
  */
 public class BuildTests {
-
+	@DisplayName("clone repo test")
+	@Test
+	void cloneRepoTest(){
+		Build b = new Build();
+		b.cloneRepo();
+		try{
+			b.cloneRepo();
+		}
+		catch (Exception e) {
+            assertTrue(e.getMessage().contains("not an empty directory"));
+		}
+	}
 }
