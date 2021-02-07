148b1f299ec9c9fa2291d6906009885c9ae8b85f
Martijn Atema <martijn@atema.one>
2021-02-07 23:31:41 +0100

    Resolves #20: Add enum for the build status and tests

---
+++ src/main/java/BuildStatus.java
@@ -0,0 +1,51 @@
+/**
+ * Contains possible states that a (completed) build can have
+ */
+public enum BuildStatus {
+    /**
+     * The build procsses completed successfully
+     */
+    SUCCESS("success"),
+
+    /**
+     * The build process completed without success
+     */
+    FAILURE("failure"),
+
+    /**
+     * The build process did not complete due to an error
+     */
+    ERROR("error");
+
+    public final String label;
+
+    private BuildStatus(String label) {
+        this.label = label;
+    }
+
+    /**
+     * Converts a BuildStatus into a string that can be easily stored or transferred
+     *
+     * @return String corresponding to this BuildStatus
+     */
+    @Override
+    public String toString() {
+        return label;
+    }
+
+    /**
+     * Returns the BuildStatus from a string created by {@link #toString()}
+     *
+     * @param label String created by {@link #toString()}
+     * @returns BuildString corresponding to the String (or ERROR)
+     */
+    public static BuildStatus fromString(String label) {
+        if (label.equals(SUCCESS.label)) {
+            return SUCCESS;
+        } else if (label.equals(FAILURE.label)) {
+            return FAILURE;
+        } else {
+            return ERROR;
+        }
+    }
+}
