291ad0e03c84ebed5c3d5639f2ecdb59ba3fa000
hallon-heyman <62335201+hallon-heyman@users.noreply.github.com>
2021-02-08 17:11:54 +0100

    #7 fix fundament for tests, wait for build implementation

--- src/test/java/BuildResultTests.java
+++ src/test/java/BuildResultTests.java
@@ -1,6 +1,24 @@
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 
 /**
  * Contains tests for the {@link BuildResult} class
  */
-
+@DisplayName("BuildResultTest Failure")
+@Test
+void calculateBuildResultFailureTest(){
+    BuildResult buildResult = new BuildResult(null, GitMessages.FAILURE);
+    buildResult.reportGitHubStatus();
+}
+@DisplayName("BuildResultTest Success")
+@Test
+void calculateBuildResultFailureTest(){
+    BuildResult buildResult = new BuildResult(null, GitMessages.SUCCESS);
+    buildResult.reportGitHubStatus();
+}
+@DisplayName("BuildResultTest Error")
+@Test
+void calculateBuildResultFailureTest(){
+    BuildResult buildResult = new BuildResult(null, GitMessages.ERROR);
+    buildResult.reportGitHubStatus();
+}
